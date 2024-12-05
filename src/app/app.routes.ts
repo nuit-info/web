e92099ba4b17b964369011af4207f135a5c1ab62
Maz0k <114288731+Maz0ky@users.noreply.github.com>
2024-12-05 23:52:38 +0100

    first_commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,7 @@
 import { Routes } from '@angular/router';
 import {MeteoComponent} from './meteo/meteo.component';
+import { CaptchaComponent } from './captcha/captcha.component';
 export const routes: Routes = [
   {path: 'meteo', component: MeteoComponent},
+  {path: 'captcha', component: CaptchaComponent},
 ];
---
+++ src/app/captcha/captcha.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-captcha',
+  standalone: true,
+  imports: [],
+  templateUrl: './captcha.component.html',
+  styleUrl: './captcha.component.css'
+})
+export class CaptchaComponent {
+
+}
