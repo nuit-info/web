0bc04059d299836a934b0d802955d6c436512ae2
Stroguytriu <dupasquierantoine@gmail.com>
2024-12-05 22:19:01 +0100

    marker

--- src/app/meteo/meteo.component.ts
+++ src/app/meteo/meteo.component.ts
@@ -1,10 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {MapsModule} from "@syncfusion/ej2-angular-maps";
-import { BrowserModule } from '@angular/platform-browser'
 import { world_map } from './world-map';
 import { registerLicense } from '@syncfusion/ej2-base';
-import { ZoomService } from '@syncfusion/ej2-angular-maps'
-
+import { ZoomService, MarkerService, SelectionService  } from '@syncfusion/ej2-angular-maps'
 
 @Component({
   selector: 'app-meteo',
@@ -12,23 +10,40 @@ import { ZoomService } from '@syncfusion/ej2-angular-maps'
   imports: [
     MapsModule
   ],
-  providers: [ZoomService],
-  template: `<ejs-maps id='maps-container' [zoomSettings] = 'zoomSettings'>
-                <e-layers>
-                    <e-layer [shapeData] = 'shapeData'></e-layer>
-                </e-layers>
-               </ejs-maps>`,
+  providers: [ZoomService, MarkerService, SelectionService],
+  templateUrl: './meteo.component.html',
   styleUrl: './meteo.component.css'
 })
 export class MeteoComponent implements OnInit{
   public shapeData: object = world_map;
   public zoomSettings?: object;
+  public markerSettings?: object;
 
   ngOnInit(): void {
     this.zoomSettings = {
       enable: true,
       toolbars: ['ZoomIn', 'ZoomOut', 'Pan', 'Reset']
     };
+    this.markerSettings = [{
+      visible: true,
+      selectionSettings: {
+        enable: true,
+        fill: 'lightblue',
+        border: { color: 'white', width: 2}
+      },
+      shapeValuePath:'shape',
+      colorValuePath:'color',
+      shape: 'Triangle',
+      fill: 'blue',
+      height: 20,
+      width: 20,
+      dataSource: [
+        { latitude: 48.085731 , longitude: -11.53778 },
+        { latitude: 35.777202 , longitude: -69.545593 },
+        { latitude: 42.165376 , longitude: -134.233093 },
+      ],
+      animationDuration: 0,
+    }]
     this.shapeData = world_map;
   }
 
@@ -36,4 +51,7 @@ export class MeteoComponent implements OnInit{
     registerLicense('Ngo9BigBOggjHTQxAR8/V1NDaF1cX2hIYVdpR2Nbek5zflROal1TVBYiSV9jS3pSdEVmWHpfeHVWR2haWQ==');
   }
 
+  onMarkerClick(args: any) {
+    console.log(args);
+  }
 }
