43697f08911ed53ba5878c92e3718b524f113830
LudovicFS <l.frealsaison@gmail.com>
2024-12-05 23:57:00 +0100

    modales pour les organes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,31 @@
 import { Component } from '@angular/core';
-import {NgOptimizedImage} from "@angular/common";
 
 @Component({
   selector: 'app-home',
-  standalone: true,
-  imports: [
-    NgOptimizedImage
-  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
 
+  openModal(organe: string, imageUrl: string) {
+    // Mettre à jour le nom de l'organe dans la modale
+    const organNameElement = document.getElementById('organName')!;
+    organNameElement.textContent = organe;
+
+    // Mettre à jour l'image dans la modale
+    const organImageElement = document.getElementById('organImage') as HTMLImageElement;
+    organImageElement.src = imageUrl;
+
+    // Afficher la modale avec une transition d'opacité
+    const modalElement = document.getElementById('organModal')!;
+    modalElement.classList.remove('opacity-0', 'pointer-events-none'); // Retirer les classes de "caché"
+    modalElement.classList.add('opacity-100', 'pointer-events-auto'); // Ajouter les classes de "visible"
+  }
+
+  closeModal() {
+    // Masquer la modale avec une transition d'opacité
+    const modalElement = document.getElementById('organModal')!;
+    modalElement.classList.remove('opacity-100', 'pointer-events-auto'); // Retirer les classes de "visible"
+    modalElement.classList.add('opacity-0', 'pointer-events-none'); // Ajouter les classes de "caché"
+  }
 }
