5cfd8eda11a93a2a93720dff697be2a49b25076d
Emre Ceylan <ceylan.emre@laposte.net>
2024-12-06 00:12:36 +0100

    captcha-test

--- src/app/captcha/captcha.component.ts
+++ src/app/captcha/captcha.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NgIf, NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-captcha',
   standalone: true,
-  imports: [],
+  imports: [
+    NgStyle,
+    NgIf
+  ],
   templateUrl: './captcha.component.html',
   styleUrl: './captcha.component.css'
 })
-export class CaptchaComponent {
+export class CaptchaComponent implements OnInit {
+
+  position = { x: 50, y: 50 };
+  captchaValidated = false;
+
+  ngOnInit(): void {
+    this.moveCircleRandomly();
+  }
+
+  handleClick(): void {
+    this.captchaValidated = true;
+  }
+
+  moveCircleRandomly(): void {
+    setInterval(() => {
+      if (!this.captchaValidated) {
+        this.position = {
+          x: Math.random() * 260,
+          y: Math.random() * 260,
+        };
+      }
+    }, 500);
+  }
 
 }
