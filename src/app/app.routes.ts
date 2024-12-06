d4029cfb646696c68db9673f884572d82b7ec652
LudovicFS <l.frealsaison@gmail.com>
2024-12-06 03:32:29 +0100

    formulaire

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import {MeteoComponent} from './meteo/meteo.component';
+import {FormulaireComponent} from './formulaire/formulaire.component';
+
 
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
-  {path: 'meteo', component: MeteoComponent}
+  {path: 'meteo', component: MeteoComponent},
+  {path: 'formulaire', component: FormulaireComponent}
 ];
