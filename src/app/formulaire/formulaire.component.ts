e2279cc4ec2a81851de040be845f7da6990f2512
HandyyWeb <lebretonandy@gmail.com>
2024-12-06 04:37:29 +0100

    Boutton qui bouge

--- src/app/formulaire/formulaire.component.ts
+++ src/app/formulaire/formulaire.component.ts
@@ -9,6 +9,7 @@ import { Component } from '@angular/core';
 })
 export class FormulaireComponent {
   inputValue: string = ''; // Variable to hold input value
+  numberClick: number = 0;
 
   // Handle input change
   onInputChange(event: Event): void {
@@ -18,6 +19,27 @@ export class FormulaireComponent {
 
   // Handle submit
   submit(): void {
+    const buttonsubmit = document.getElementById('buttonsubmit')!;
+
+    if (this.numberClick < 3) {
+      this.numberClick += 1;
+
+      const newX = Math.trunc(Math.random() * window.innerWidth);
+      const newY = Math.trunc(Math.random() * window.innerHeight);
+
+      console.log(newX, newY);
+      console.log(`translate-x-[${newX}px]`, `translate-y-[${newY}px]`);
+
+      buttonsubmit.style.transform = `translate(${newX}px, ${newY}px)`;
+
+      // buttonsubmit.classList.add(
+      //   `translate-x-[${newX}px]`,
+      //   `translate-y-[${newY}px]`
+      // );
+
+      // buttonsubmit.classList.add('translate-x-[5px]', 'translate-y-[450px]');
+    }
+
     console.log('Input value:', this.inputValue); // Log input value on submit
   }
 }
